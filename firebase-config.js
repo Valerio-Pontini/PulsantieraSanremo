9a66dfa828378fc2575d9d5f6146d17026f4d65f
Valerio-Pontini <valerio.pontini@gmail.com>
2026-02-24 17:25:19 +0100

    Add Firebase configuration to firebase-config.js
    Add Firebase configuration settings for the project.

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAoXKuDub4W4SI7zrgqfFEl7VrFiv6tx-g",
+  authDomain: "sanremo2026-bcb4c.firebaseapp.com",
+  projectId: "sanremo2026-bcb4c",
+  storageBucket: "sanremo2026-bcb4c.firebasestorage.app",
+  messagingSenderId: "414097254592",
+  appId: "1:414097254592:web:f2d0cdacf2e66edcf46525",
+  measurementId: "G-5YC92NEW91"
+};
